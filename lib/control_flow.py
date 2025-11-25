fec8b64a6d02fa7b7c758bf3c2f2e2861d73dbc3
yvonnekadenyi-m <kadenyimasheti@gmail.com>
2025-11-25 18:40:15 +0300

    added to lib

--- lib/control_flow.py
+++ lib/control_flow.py
@@ -6,3 +6,15 @@ def admin_login(username, password):
         return "Access denied"
 
 
+
+def hows_the_weather(temperature):
+    if temperature < 40:
+        return "It's brisk out there!"
+    elif temperature < 65:
+        return "It's a little chilly out there!"
+    elif temperature > 85:
+        return "It's too dang hot out there!"
+    else:
+        return "It's perfect out there!"
+
+
