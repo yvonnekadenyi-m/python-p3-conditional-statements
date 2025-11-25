873d2fdce2d32f3e539305963f3f1776bf5fb704
yvonnekadenyi-m <kadenyimasheti@gmail.com>
2025-11-25 18:41:23 +0300

    added to lib

--- lib/control_flow.py
+++ lib/control_flow.py
@@ -18,3 +18,12 @@ def hows_the_weather(temperature):
         return "It's perfect out there!"
 
 
+def fizzbuzz(num):
+    if num % 3 == 0 and num % 5 == 0:
+        return "FizzBuzz"
+    elif num % 3 == 0:
+        return "Fizz"
+    elif num % 5 == 0:
+        return "Buzz"
+    else:
+        return num
