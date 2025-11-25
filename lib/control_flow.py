d660a9fbce51c71e2e0b92cbbd066e7cf1557ea1
yvonnekadenyi-m <kadenyimasheti@gmail.com>
2025-11-25 18:42:42 +0300

    added to lib

--- lib/control_flow.py
+++ lib/control_flow.py
@@ -27,3 +27,17 @@ def fizzbuzz(num):
         return "Buzz"
     else:
         return num
+
+
+def calculator(operation, num1, num2):
+    if operation == "+":
+        return num1 + num2
+    elif operation == "-":
+        return num1 - num2
+    elif operation == "*":
+        return num1 * num2
+    elif operation == "/":
+        return num1 / num2
+    else:
+        print("Invalid operation!")
+        return None
