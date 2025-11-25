05e3c081d5ac0721ec3468d6d1fb9f2c56b4d960
yvonnekadenyi-m <kadenyimasheti@gmail.com>
2025-11-25 18:39:32 +0300

    added to lib

--- lib/control_flow.py
+++ lib/control_flow.py
@@ -1,17 +1,8 @@
-#!/usr/bin/env python3
 
 def admin_login(username, password):
-    # your code here
-    pass
+    if username.lower() == "admin" and password == "12345":
+        return "Access granted"
+    else:
+        return "Access denied"
 
-def hows_the_weather(temperature):
-    # your code here
-    pass
 
-def fizzbuzz(num):
-    # your code here
-    pass
-
-def calculator(operation, num1, num2):
-    # your code here
-    pass
